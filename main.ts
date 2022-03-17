b3d2a3c85fe448a40671992ace3305c5dc267ad7
Tinalin.Lin <Tinalin.Lin@advantech.com.tw>
2022-03-17 16:09:36 +0800

    Update | package.json | add bootstrap and bootstrap-vue

--- main.ts
+++ main.ts
@@ -11,8 +11,17 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoReplaceSvg = false;
 
+// CSS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.min.css';
+
 // Vue config
 Vue.config.productionTip = false;
+
+// install bootstrap ui support
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
 
 new Vue({
     router,
