d6eaa8878adcb169a6d035989cba07f4addd9ea9
Tinalin.Lin <Tinalin.Lin@advantech.com.tw>
2022-03-28 14:34:40 +0800

    Update | component form-builder | update forder path

--- src/components/form-builders/elements/index.ts
+++ src/components/form-builders/elements/index.ts
@@ -1,51 +1,51 @@
 //#region
-import FormBuilderDisplayTextSetting from '@/components/form-builders/elements/display-text/index-setting.vue';
-import FormBuilderDisplayText from '@/components/form-builders/elements/display-text/index.vue';
+import FormBuilderDisplayTextSetting from '@/components/form-builders/elements/display/display-text/index-setting.vue';
+import FormBuilderDisplayText from '@/components/form-builders/elements/display/display-text/index.vue';
 
-import FormBuilderDisplayImageSetting from '@/components/form-builders/elements/display-image/index-setting.vue';
-import FormBuilderDisplayImage from '@/components/form-builders/elements/display-image/index.vue';
+import FormBuilderDisplayImageSetting from '@/components/form-builders/elements/display/display-image/index-setting.vue';
+import FormBuilderDisplayImage from '@/components/form-builders/elements/display/display-image/index.vue';
 //#endregion
 
 //#region
-import FormBuilderInputSingleTextSetting from '@/components/form-builders/elements/input-single-text/index-setting.vue';
-import FormBuilderInputSingleText from '@/components/form-builders/elements/input-single-text/index.vue';
+import FormBuilderInputSingleTextSetting from '@/components/form-builders/elements/input/input-single-text/index-setting.vue';
+import FormBuilderInputSingleText from '@/components/form-builders/elements/input/input-single-text/index.vue';
 
-import FormBuilderInputMultipleTextSetting from '@/components/form-builders/elements/input-multiple-text/index-setting.vue';
-import FormBuilderInputMultipleText from '@/components/form-builders/elements/input-multiple-text/index.vue';
+import FormBuilderInputMultipleTextSetting from '@/components/form-builders/elements/input/input-multiple-text/index-setting.vue';
+import FormBuilderInputMultipleText from '@/components/form-builders/elements/input/input-multiple-text/index.vue';
 
-import FormBuilderInputNumberSetting from '@/components/form-builders/elements/input-number/index-setting.vue';
-import FormBuilderInputNumber from '@/components/form-builders/elements/input-number/index.vue';
+import FormBuilderInputNumberSetting from '@/components/form-builders/elements/input/input-number/index-setting.vue';
+import FormBuilderInputNumber from '@/components/form-builders/elements/input/input-number/index.vue';
 
-import FormBuilderInputTextEditorSetting from '@/components/form-builders/elements/input-text-editor/index-setting.vue';
-import FormBuilderInputTextEditor from '@/components/form-builders/elements/input-text-editor/index.vue';
+import FormBuilderInputTextEditorSetting from '@/components/form-builders/elements/input/input-text-editor/index-setting.vue';
+import FormBuilderInputTextEditor from '@/components/form-builders/elements/input/input-text-editor/index.vue';
 //#endregion
 
 //#region
-import FormBuilderPickDropdownListSetting from '@/components/form-builders/elements/pick-dropdown-list/index-setting.vue';
-import FormBuilderPickDropdownList from '@/components/form-builders/elements/pick-dropdown-list/index.vue';
+import FormBuilderPickDropdownListSetting from '@/components/form-builders/elements/pick/pick-dropdown-list/index-setting.vue';
+import FormBuilderPickDropdownList from '@/components/form-builders/elements/pick/pick-dropdown-list/index.vue';
 
-import FormBuilderPickRadioSetting from '@/components/form-builders/elements/pick-radio/index-setting.vue';
-import FormBuilderPickRadio from '@/components/form-builders/elements/pick-radio/index.vue';
+import FormBuilderPickRadioSetting from '@/components/form-builders/elements/pick/pick-radio/index-setting.vue';
+import FormBuilderPickRadio from '@/components/form-builders/elements/pick/pick-radio/index.vue';
 
-import FormBuilderPickCheckboxSetting from '@/components/form-builders/elements/pick-checkbox/index-setting.vue';
-import FormBuilderPickCheckbox from '@/components/form-builders/elements/pick-checkbox/index.vue';
+import FormBuilderPickCheckboxSetting from '@/components/form-builders/elements/pick/pick-checkbox/index-setting.vue';
+import FormBuilderPickCheckbox from '@/components/form-builders/elements/pick/pick-checkbox/index.vue';
 
-import FormBuilderPickSwitchSetting from '@/components/form-builders/elements/pick-switch/index-setting.vue';
-import FormBuilderPickSwitch from '@/components/form-builders/elements/pick-switch/index.vue';
+import FormBuilderPickSwitchSetting from '@/components/form-builders/elements/pick/pick-switch/index-setting.vue';
+import FormBuilderPickSwitch from '@/components/form-builders/elements/pick/pick-switch/index.vue';
 
-import FormBuilderPickSliderSetting from '@/components/form-builders/elements/pick-slider/index-setting.vue';
-import FormBuilderPickSlider from '@/components/form-builders/elements/pick-slider/index.vue';
+import FormBuilderPickSliderSetting from '@/components/form-builders/elements/pick/pick-slider/index-setting.vue';
+import FormBuilderPickSlider from '@/components/form-builders/elements/pick/pick-slider/index.vue';
 
-import FormBuilderPickTimeSetting from '@/components/form-builders/elements/pick-time/index-setting.vue';
-import FormBuilderPickTime from '@/components/form-builders/elements/pick-time/index.vue';
+import FormBuilderPickTimeSetting from '@/components/form-builders/elements/pick/pick-time/index-setting.vue';
+import FormBuilderPickTime from '@/components/form-builders/elements/pick/pick-time/index.vue';
 
-import FormBuilderPickDateSetting from '@/components/form-builders/elements/pick-date/index-setting.vue';
-import FormBuilderPickDate from '@/components/form-builders/elements/pick-date/index.vue';
+import FormBuilderPickDateSetting from '@/components/form-builders/elements/pick/pick-date/index-setting.vue';
+import FormBuilderPickDate from '@/components/form-builders/elements/pick/pick-date/index.vue';
 //#endregion
 
 //#region
-import FormBuilderLayoutDividerSetting from '@/components/form-builders/elements/layout-divider/index-setting.vue';
-import FormBuilderLayoutDivider from '@/components/form-builders/elements/layout-divider/index.vue';
+import FormBuilderLayoutDividerSetting from '@/components/form-builders/elements/layout/layout-divider/index-setting.vue';
+import FormBuilderLayoutDivider from '@/components/form-builders/elements/layout/layout-divider/index.vue';
 //#endregion
 
 export enum EElementType {
